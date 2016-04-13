7e364967452c39cd0813525f9a6525f7d86ae860
Joshua Erickson <joshua.erickson@audaxhealth.com>
2016-04-13 12:58:08 -0700

    Redo the url title to accept GET requests

--- api.py
+++ api.py
@@ -12,32 +12,52 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "test"
-@app.route("/url/title", methods=['POST'])
+
+
+
+@app.route("/url/title", methods=['GET', 'POST'])
 def title():
     """Returns the title for a URL. The URL is passed using JSON"""
-    try:
-        json_data = request.get_json()
-    except:
-        return "Must POST json with a value for URL"
-    if "URL" in json_data:
+    if request.method == 'POST': 
         try:
-            response = urllib2.urlopen(json_data['URL'])
-            url_data = response.read()
+            json_data = request.get_json()
         except:
-            return "Couldn't connect to url %(URL)s" % json_data
-        try:
-            soup = BeautifulSoup(url_data)
-            title = soup.title.string
-        except:
-            mo = re.search(r'<title>(.*)</title>', url_data)
-            if mo:
-                title = mo.group(1)
-            else:
-                title = ""
-        parser = HTMLParser.HTMLParser()
-        return parser.unescape(title)
+            return "Must POST json with a value for URL"
+        if "URL" in json_data:
+            return get_url_title(json_data['URL'])
+        else:
+            return "JSON must contain URL field"
     else:
-        return "JSON must contain URL field"
+        url = request.args.get('URL', None)
+        if url:
+            return get_url_title(urllib2.unquote(url))
+        else:
+            return "No URL specified"
+
+
+
+
+
+def get_url_title(url):
+    try:
+        response = urllib2.urlopen(url)
+        url_data = response.read()
+    except:
+        return "Couldn't connect to url %s" % url
+    try:
+        soup = BeautifulSoup(url_data)
+        title = soup.title.string
+    except:
+        mo = re.search(r'<title>(.*)</title>', url_data)
+        if mo:
+            title = mo.group(1)
+        else:
+            title = ""
+    parser = HTMLParser.HTMLParser()
+    return parser.unescape(title)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+
